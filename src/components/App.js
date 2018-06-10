d038e8ddd64caf01a003a2a9008596f7ee9591ed
Ctharp11 <ctharp1988@gmail.com>
2018-06-10 12:33:19 -0500

    added nav

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+// COMPONENTS
+import Home from './Home';
+import Nav from './Nav';
 
 class App extends Component {
     render() {
         return(
             <div>
-                <div className="app"> Sup </div>
-                <div className="home"> Home </div>
+                <Nav />
+                <Switch>
+                    <Route path="/" component={Home} /> 
+                </Switch>
             </div>
         )
     }
---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Nav = () => (
+    <div className="nav">  
+        <div className="nav-header"> <Link to="/"> Front End Musings </Link> </div> 
+
+    </div>
+)
+
+export default Nav;
