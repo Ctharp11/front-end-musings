e7ab1dc6dacefce6fcef918a24b704d85e14c927
Ctharp11 <ctharp1988@gmail.com>
2018-06-10 12:05:17 -0500

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return(
+            <div>
+                <div className="app"> Sup </div>
+                <div className="home"> Home </div>
+            </div>
+        )
+    }
+}
+
+export default App;
